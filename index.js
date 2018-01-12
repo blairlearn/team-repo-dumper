b79a68a224cd32218e1af651c070c404b7be2e12
Blair Learn <blairlearn@users.noreply.github.com>
2018-01-12 10:23:51 -0500

    Initial setup.  Dump a list of teams.

---
+++ index.js
@@ -0,0 +1,29 @@
+var GITHUB_TOKEN = "{{PERSONAL ACCESS TOKEN GOES HERE}}}";
+
+const https = require('https');
+
+var options = {
+    //'https://api.github.com/orgs/nciocpl/teams';
+    protocol: 'https:',
+    hostname: 'api.github.com',
+    path: '/orgs/nciocpl/teams',
+    method: 'GET',
+    headers: {
+        'User-Agent': 'Repo Dumper',
+        'Authorization': `token ${GITHUB_TOKEN}`
+    }
+};
+
+
+https.get(options, (res) => {
+
+    //console.log('statusCode:', res.statusCode);
+    //console.log('headers:', res.headers);
+
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
+
+}).on('error', (e) => {
+    console.error(e);
+});
