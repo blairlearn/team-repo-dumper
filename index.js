848a1924b67c6d43629dcb3e27e02a0535cf83c6
Blair Learn <blairlearn@users.noreply.github.com>
2018-01-12 11:51:20 -0500

    Dump per-team repo IDs and names.

--- index.js
+++ index.js
@@ -1,36 +1,65 @@
-var GITHUB_TOKEN = process.env["GITHUB_TOKEN"];
-
+const GITHUB_TOKEN = process.env["GITHUB_TOKEN"];
 const https = require('https');
 
-var options = {
-    //'https://api.github.com/orgs/nciocpl/teams';
-    protocol: 'https:',
-    hostname: 'api.github.com',
-    path: '/orgs/nciocpl/teams',
-    method: 'GET',
-    headers: {
-        'User-Agent': 'Repo Dumper',
-        'Authorization': `token ${GITHUB_TOKEN}`
-    }
-};
+GetTeams();
 
+function GetTeams() {
+    var options = {
+        //'https://api.github.com/orgs/nciocpl/teams';
+        protocol: 'https:',
+        hostname: 'api.github.com',
+        path: '/orgs/nciocpl/teams',
+        method: 'GET',
+        headers: {
+            'User-Agent': 'Repo Dumper',
+            'Authorization': `token ${GITHUB_TOKEN}`
+        }
+    };
+    https.get(options, (res) => {
+        let teamData = '';
+        res.on('data', (d) => {
+            teamData += d;
+        });
+        res.on('end', () => {
+            let teams = JSON.parse(teamData);
+            teams.forEach(team => {
+                DumpReposForTeam(team.id, team.name);
+            });
+        });
+    }).on('error', (e) => {
+        console.error(e);
+    });
+}
 
-https.get(options, (res) => {
+function DumpReposForTeam(teamid, teamName) {
 
-    let teamData = '';
-    
-    res.on('data', (d) => {
-        teamData += d;
+    var options = {
+        protocol: 'https:',
+        hostname: 'api.github.com',
+        path: `/teams/${teamid}/repos`,
+        method: 'GET',
+        headers: {
+            'User-Agent': 'Repo Dumper',
+            'Authorization': `token ${GITHUB_TOKEN}`
+        }
+    };
+
+    let repoData = '';
+
+    https.get(options, (res) => {
+        let teamData = '';
+        res.on('data', (d) => {
+            repoData += d;
+        });
+        res.on('end', () => {
+            let repos = JSON.parse(repoData);
+            repos.forEach(repo => {
+                console.log(repo.id + '\t' + repo.name);
+            });
+        });
+    }).on('error', (e) => {
+        console.error(e);
     });
 
-    res.on('end', () =>{
-        teams = JSON.parse(teamData);
-        teams.forEach(team => {
-            process.stdout.write(team.id + "\t" + team.name + "\n");
-        });
+}
 
-    })
-
-}).on('error', (e) => {
-    console.error(e);
-});
